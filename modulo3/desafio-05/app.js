2bd4c67d62b0a2124cb760f3a526083f6221cad3
matheus mayer <matheus.mayer@assert.ifpb.edu.br>
2022-02-28 00:22:50 -0300

    Desafio 5

---
+++ modulo3/desafio-05/app.js
@@ -0,0 +1,24 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		wannaSee: true,
+		orNot: false
+	},
+	methods: {
+		alternarView(){
+			this.wannaSee = !this.wannaSee
+			this.orNot = !this.orNot
+		}
+	}
+});
