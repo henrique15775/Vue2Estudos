a21fff0df3bcae3228c5775f8eae3af3d47e457f
matheus mayer <matheus.mayer@assert.ifpb.edu.br>
2022-03-05 18:06:14 -0300

    Modulo 7

---
+++ modulo7/componente-intro/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <app-contadores />
+    <button @click="contador += '#'">?</button>
+    <p>{{ contador }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+    contador: ""}
+  }
+ 
+}
+</script>
+
+<style>
+
+</style>
