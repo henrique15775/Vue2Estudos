94e5e2b2ef2ac73942db1fe67c7dd4a474369b0b
matheus mayer <matheus.mayer@assert.ifpb.edu.br>
2022-03-08 18:12:30 -0300

    Modulo 8

---
+++ modulo8/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p>Nome -> {{ nome }}</p>
+        <hr>
+        
+        <div class="componentes">
+            <app-usuario-info :nome="nome" @nomeMudou="nome = $event" />
+            <app-usuario-editar />
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+async created() {
+    const response = await fetch("https://api.npms.io/v2/search?q=vue")
+    const data = await response.json()
+    this.totalVuePackages = data.total
+  },data(){
+        return {
+            nome: ""
+        }
+    },
+    components: { AppUsuarioInfo, AppUsuarioEditar }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+        border: 1px black;
+        border-radius:6px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ modulo8/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <button @click="alterarNome()"> Alterar Nome </button>
+        <p>Nome do usuário: {{ nome }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        nome: [String, Array]
+    },
+    methods:{
+        alterarNome(){
+            this.nome="Kkkkk",
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1; 
+        background-color: #ec485f;
+        color: #fff;
+        border-radius: 6px;
+    }
+</style>
